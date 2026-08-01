0aa76cce9ec33ac90ba75b3779e7c5f250c7c207
jhin0410-lgtm <jhin0410@gmail.com>
2026-08-01 20:26:51 +0900

    Use robust battery run audit wrapper

--- src/platform_core/battery_intelligence/__init__.py
+++ src/platform_core/battery_intelligence/__init__.py
@@ -11,10 +11,8 @@ from .forecast_baselines import build_baseline_predictions
 from .forecast_validation import evaluate_grouped_forecast
 from .error_diagnostics import build_error_diagnostics
 from .raw_signal_admission import audit_raw_signal_admission
-from .target_comparability import (
-    audit_battery_intelligence_run,
-    build_target_comparability_audit,
-)
+from .target_comparability import build_target_comparability_audit
+from .target_comparability_runner import audit_battery_intelligence_run
 from .closeout import scientific_closeout
 from .workflow import run_battery_intelligence
 from .nasa_pcoe_resilient import import_nasa_pcoe_battery
