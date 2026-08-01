232923016742209a37f0f1e583c99d4985d78f54
jhin0410-lgtm <jhin0410@gmail.com>
2026-08-01 16:39:36 +0900

    Export battery diagnostics and admission APIs

--- src/platform_core/battery_intelligence/__init__.py
+++ src/platform_core/battery_intelligence/__init__.py
@@ -7,7 +7,10 @@ from .common import (
 from .degradation import analyze_trajectories, detect_knee_point
 from .signals import extract_signal_features
 from .forecast_table import build_forecast_table
+from .forecast_baselines import build_baseline_predictions
 from .forecast_validation import evaluate_grouped_forecast
+from .error_diagnostics import build_error_diagnostics
+from .raw_signal_admission import audit_raw_signal_admission
 from .closeout import scientific_closeout
 from .workflow import run_battery_intelligence
 
@@ -19,7 +22,10 @@ __all__ = [
     "detect_knee_point",
     "analyze_trajectories",
     "build_forecast_table",
+    "build_baseline_predictions",
     "evaluate_grouped_forecast",
+    "build_error_diagnostics",
+    "audit_raw_signal_admission",
     "scientific_closeout",
     "run_battery_intelligence",
 ]
